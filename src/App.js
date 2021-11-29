0377ef9a373c60967030c96d59c9da4e4a5bb092
Volodymyr Gotsak <vgotsak29@gmail.com>
2021-11-29 14:05:48 +0200

    My firs React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 3 
+    }
+  }
+
+  plusCounter = () => {
+    this.setState(state => ({
+      counter: state.counter + 1
+    }))
+  };
+
+  minusCounter = () => {
+    if (this.state.counter >= 1) {
+      this.setState(state => ({
+        counter: state.counter - 1
+      }))
+    }
+  };
+
+  randomCounter = () => {
+    this.setState({
+      counter: +(Math.random() * (100 - 0) + 0).toFixed(0)
+    })
+  };
+
+  resetCounter = () => {
+    this.setState({
+      counter: this.props.counter
+    })
+  };
+
+
+  render() {
+    
+    return (
+      <div className="App">
+      <div className="counter">
+        {this.state.counter}
+      </div>
+      <div className="buttons">
+        <button onClick={this.plusCounter}>Plus</button>
+        <button onClick={this.minusCounter}>Minus</button>
+        <button onClick={this.randomCounter}>Random</button>
+        <button onClick={this.resetCounter}>Reset</button>
+      </div>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
